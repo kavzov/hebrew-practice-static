4500cc35514eef9cf698af7356f6b5addfd0d7fe
Igor Kavzov <igor.kavzov@gmail.com>
2018-04-16 12:55:43 +0300

    Alphabet initial

---
+++ static/js/alphabet.js
@@ -0,0 +1,43 @@
+function letterOutput() {
+    var letter = [
+        {'he': 'א‬', 'ru': 'Áлеф'}, {'he': 'בּ‬‬', 'ru': 'Бет'}, {'he': 'ב‬', 'ru': 'Вет'}, {'he': 'ג‬', 'ru': 'Ги́мель'},
+        {'he': 'ד‬', 'ru': 'Да́лет'}, {'he': 'ה‬', 'ru': 'hей'}, {'he': 'ו‬', 'ru': 'вав'}, {'he': 'ז‬', 'ru': 'За́ин'},
+        {'he': 'ח‬', 'ru': 'Хет'}, {'he': 'ט‬', 'ru': 'Тет'}, {'he': 'י‬', 'ru': 'Йуд'}, {'he': 'כּ‬', 'ru': 'Каф'},
+        {'he': 'כ‬', 'ru': 'Хаф'}, {'he': 'ך‬', 'ru': 'Хаф софи́т'}, {'he': 'ל‬', 'ru': 'Ла́мед'}, {'he': 'מ‬', 'ru': 'Мем'},
+        {'he': 'ם‬', 'ru': 'Мем софи́т'}, {'he': 'נ‬', 'ru': 'Нун'}, {'he': 'ן‬', 'ru': 'Нун софи́т'}, {'he': 'ס‬', 'ru': 'Са́мех'},
+        {'he': 'ע‬', 'ru': 'А́ин'}, {'he': 'פּ‬', 'ru': 'Пей'}, {'he': 'פ‬', 'ru': 'Фей'}, {'he': 'ף‬', 'ru': 'Фей софи́т'},
+        {'he': 'צ‬', 'ru': 'Ца́ди'}, {'he': 'ץ‬', 'ru': 'Ца́ди софи́т'}, {'he': 'ק‬', 'ru': 'Куф'}, {'he': 'ר‬', 'ru': 'Рейш'},
+        {'he': 'שׁ‬', 'ru': 'Шин'}, {'he': 'שׂ‬‬', 'ru': 'Син'}, {'he': 'ת‬', 'ru': 'Тав'}
+    ];
+
+    var lastLetter = $('#hebrew-letter').text();
+    var rand = getRandInt(0, letter.length);
+    var hebLetter = letter[rand]['he'];
+
+    while (lastLetter === hebLetter) {
+        rand = getRandInt(0, letter.length);
+        hebLetter = letter[rand]['he'];
+    }
+    $('#hebrew-letter').html(hebLetter);
+    $('#answ-letter-rus').html(letter[rand]['ru']);
+    /* show button title */
+    $('#answ-btn').val(btnTitleTransl);
+    /* disable green color of the button if there is */
+    if ($('#answ-btn').hasClass('btn-success')) $('#answ-btn').removeClass('btn-success');
+    /* hide answer */
+    $('#letter-answ').hide();
+}
+
+function letterAnswerClick() {
+    if ($('#letter-answ').css('display') == 'none') {
+        $('#letter-answ').fadeIn(250);
+        $('#answ-btn').val(btnTitleMore);
+        $('#answ-btn').addClass('btn-success');
+    }
+    else {
+        letterOutput();
+    }
+}
+
+/* answer button click */
+$('#answ-btn').on('click', function() { letterAnswerClick() });
