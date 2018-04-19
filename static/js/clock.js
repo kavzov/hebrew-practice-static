234719088eb516c92099f610ff29d398036035f3
Igor Kavzov <igor.kavzov@gmail.com>
2018-04-19 18:13:13 +0300

    Clock page refactoring

---
+++ static/js/clock.js
@@ -0,0 +1,83 @@
+/** Functions **/
+function getRandHours() {
+    return getRandInt(0, 12);
+}
+
+function getRandMinutes(multiple) {
+    let minutes = getRandInt(0, 60);
+    // Counts with multiple if it set
+    if (multiple)
+        while (minutes % multiple !== 0) {
+            minutes = getRandInt(0, 60);
+        }
+    return minutes;
+}
+
+function dakot(lang) {
+    /* Returns 'dakot' (minutes) word on `lang` or on hebrew */
+    return (isTranslitLang(lang)) ? ' дакóт ' : ' דַקוֹת ';
+}
+
+function set0hrsTo00(hours) {
+    return (hours) ? hours : '00';
+}
+
+function setMinutesLeadZero(minutes) {
+    return (minutes < 10) ? '0' + minutes : minutes;
+}
+
+function formTimeStr(hrs, min) {
+    return hrs + ':' + min;
+}
+
+function translateTime(hrs, min, lang) {
+    /* Translates `hrs` and `min` to `lang` and forms a string */
+    let str = '';
+    if (min < 40) {
+        str = translateNumber(hrs, lang);
+        if (min === 0) {
+            str += (lang === 'rus') ? ' бидъю́к' : ' בדיוק';
+        } else if (min <= 20) {
+            if (min === 15) {
+                str += (lang === 'rus') ? ' ва рэ́ва' : ' וַרֶבַע';
+            } else {
+                str += getNumPrep(min, lang);
+                str += translateNumber(min, lang);
+                if (min !== 20) str += dakot(lang);
+            }
+        } else {
+            if (min === 30) {
+                str += (lang === 'rus') ? ' ва хэ́ци' : ' וַחֵצִי';
+            } else {
+                str += ' ' + translateNumber(min, lang) + dakot(lang);  /* make male minutes */
+            }
+        }
+    } else {
+        if (min === 45) {
+            str = (lang === 'rus') ? 'рэ́ва ' : 'רֶבַע ';
+        } else {
+            str = translateNumber(60 - min, lang) + dakot(lang);
+        }
+        str += (lang === 'rus') ? 'лэ ' : 'לְ';
+        str += translateNumber(hrs + 1, lang);
+    }
+    return str;
+}
+
+function outputContent() {
+    /* Outputs digital time, button title, translation (and hide it), ungreencolorize button (if there is) */
+    let hours = getRandHours();
+    let minutes = getRandMinutes(5);
+    let translit = translateTime(hours, minutes, 'rus');
+    let hebrew = translateTime(hours, minutes, 'heb');
+    hours = set0hrsTo00(hours);                // '0' hours -> '00'
+    minutes = setMinutesLeadZero(minutes);     // 'N' min -> '0N'
+    let taskStr = formTimeStr(hours, minutes);
+
+    outputTask(taskStr);
+    setBtnTitle(translateBtn, btnTitleTranslate);
+    removeBtnColor(translateBtn, greenColor);  // disable green color of the button if it set before
+    outputTranslation(translit, hebrew);
+    hideTranslation();
+}
+
---
+++ static/js/main.js
@@ -0,0 +1,104 @@
+/** Vars **/
+let toLang = 'heb';
+let translitLang = 'rus';
+let btnTitleTranslate = 'Перевод';
+let btnTitleMore = 'Ещё';
+
+
+/** Objects **/
+let page = $(document);
+let taskArea = $('#task');
+let translateBtn = $('#translate-btn').find('>input');
+let translationArea = $('#translation');
+
+
+/** Colors (bootstrap buttons, alerts etc.) **/
+let redColor = 'red'; let greenColor = 'green'; let blueColor = 'blue'; let yellowColor = 'yellow';
+let aquaColor = 'aqua'; let darkGreyColor = 'darkGrey'; let greyColor = 'grey'; let lightGreyColor = 'lightGrey';
+let btnColorClass = {
+    'red': 'btn-danger', 'green': 'btn-success', 'blue': 'btn-primary', 'yellow': 'btn-warning',
+    'aqua': 'btn-info', 'darkGrey': 'btn-dark', 'grey': 'btn-secondary', 'lightGrey': 'btn-light'
+};
+
+
+/** Functions **/
+function isTranslitLang(lang) {
+    return lang === translitLang;
+}
+
+function outputHTML(node, html) {
+    $(node).html(html);
+}
+
+function outputTask(taskStr) {
+    outputHTML(taskArea, taskStr);
+}
+
+function outputTranslation(translit, hebrew) {
+    let translitArea = translationArea.find('.translit');
+    let hebrewArea = translationArea.find('.hebrew');
+    outputHTML(translitArea, translit);
+    outputHTML(hebrewArea, hebrew);
+}
+
+function setBtnColor(btn, color) {
+    $(btn).addClass(btnColorClass[color]);
+}
+
+function removeBtnColor(btnNode, color) {
+    $(btnNode).removeClass(btnColorClass[color]);
+}
+
+function hideNode(node, time) {
+    time = time || 0;
+    $(node).hide(time);
+}
+
+function hideTranslation() {
+    hideNode(translationArea);
+}
+
+function nodeIsDisplayed(node) {
+    /* Checks whether a node is displayed or not */
+    return $(node).css('display') !== 'none';
+}
+
+function translationIsHidden() {
+    /* Check whether the translation is hidden */
+    return !nodeIsDisplayed(translationArea);
+}
+
+function showNode(node, time) {
+    /* Show not displayed node within `time` ms */
+    time = time || 0;
+    $(node).fadeIn(time);
+}
+
+function showTranslation() {
+    showNode(translationArea, 250);
+}
+
+function setNodeValue(node, val) {
+    $(node).val(val);
+}
+
+function setBtnTitle(btnNode, title) {
+    setNodeValue(btnNode, title);
+}
+
+function showTranslationOrNewTask() {
+    /* Shows task translation or a new task */
+    if (translationIsHidden(translationArea)) {
+        showTranslation(translationArea);
+        setBtnColor(translateBtn, greenColor);
+        setBtnTitle(translateBtn, btnTitleMore);
+    } else {
+        outputContent();
+    }
+}
+
+/** Events handlers **/
+/* Page load */
+page.ready(outputContent());
+/* Click on Translate button */
+translateBtn.on('click', function() { showTranslationOrNewTask(); });
