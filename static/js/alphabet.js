17abc70acec18f47fb52e1427d7a99ab806b5d1d
Igor Kavzov <igor.kavzov@gmail.com>
2018-04-20 18:21:17 +0300

    Alphabet refactoring

--- static/js/alphabet.js
+++ static/js/alphabet.js
@@ -1,43 +1,50 @@
-function letterOutput() {
-    var letter = [
-        {'he': 'א‬', 'ru': 'Áлеф'}, {'he': 'בּ‬‬', 'ru': 'Бет'}, {'he': 'ב‬', 'ru': 'Вет'}, {'he': 'ג‬', 'ru': 'Ги́мель'},
-        {'he': 'ד‬', 'ru': 'Да́лет'}, {'he': 'ה‬', 'ru': 'hей'}, {'he': 'ו‬', 'ru': 'вав'}, {'he': 'ז‬', 'ru': 'За́ин'},
-        {'he': 'ח‬', 'ru': 'Хет'}, {'he': 'ט‬', 'ru': 'Тет'}, {'he': 'י‬', 'ru': 'Йуд'}, {'he': 'כּ‬', 'ru': 'Каф'},
-        {'he': 'כ‬', 'ru': 'Хаф'}, {'he': 'ך‬', 'ru': 'Хаф софи́т'}, {'he': 'ל‬', 'ru': 'Ла́мед'}, {'he': 'מ‬', 'ru': 'Мем'},
-        {'he': 'ם‬', 'ru': 'Мем софи́т'}, {'he': 'נ‬', 'ru': 'Нун'}, {'he': 'ן‬', 'ru': 'Нун софи́т'}, {'he': 'ס‬', 'ru': 'Са́мех'},
-        {'he': 'ע‬', 'ru': 'А́ин'}, {'he': 'פּ‬', 'ru': 'Пей'}, {'he': 'פ‬', 'ru': 'Фей'}, {'he': 'ף‬', 'ru': 'Фей софи́т'},
-        {'he': 'צ‬', 'ru': 'Ца́ди'}, {'he': 'ץ‬', 'ru': 'Ца́ди софи́т'}, {'he': 'ק‬', 'ru': 'Куф'}, {'he': 'ר‬', 'ru': 'Рейш'},
-        {'he': 'שׁ‬', 'ru': 'Шин'}, {'he': 'שׂ‬‬', 'ru': 'Син'}, {'he': 'ת‬', 'ru': 'Тав'}
+/** Functions **/
+function outputContent() {
+    let letters = [
+        {'letter': 'א', 'heb': 'אָלֶף‬‬', 'rus': 'Áлеф'}, {'letter': 'בּ‬‬', 'heb': 'בֵּית‬', 'rus': 'Бет'},
+        {'letter': 'ב‬', 'heb': 'בֵית‬', 'rus': 'Вет'}, {'letter': 'ג‬', 'heb': 'גִּימֵל‬', 'rus': 'Ги́мель'},
+        {'letter': 'ד‬', 'heb': 'דָּלֶת‬', 'rus': 'Да́лет'}, {'letter': 'ה‬', 'heb': 'הֵא‬', 'rus': 'hей'},
+        {'letter': 'ו‬', 'heb': 'וָו‬', 'rus': 'вав'}, {'letter': 'ז‬', 'heb': 'זַיִן‬', 'rus': 'За́ин'},
+        {'letter': 'ח‬', 'heb': 'חֵית‬', 'rus': 'Хет'}, {'letter': 'ט‬', 'heb': 'טֵית‬', 'rus': 'Тет'},
+        {'letter': 'י‬', 'heb': 'יוֹד‬', 'rus': 'Йод'}, {'letter': 'כּ‬', 'heb': 'כַּף‬', 'rus': 'Каф'},
+        {'letter': 'כ‬', 'heb': 'כַף‬', 'rus': 'Хаф'}, {'letter': 'ך‬', 'heb': 'כַף סוֹפִית‬', 'rus': 'Хаф софи́т'},
+        {'letter': 'ל‬', 'heb': 'לָמֶד‬', 'rus': 'Ла́мед'}, {'letter': 'מ‬', 'heb': 'מֵם‬', 'rus': 'Мем'},
+        {'letter': 'ם‬', 'heb': 'מֵם סוֹפִית‬', 'rus': 'Мем софи́т'}, {'letter': 'נ‬', 'heb': 'נוּן‬', 'rus': 'Нун'},
+        {'letter': 'ן‬', 'heb': 'נוּן סוֹפִית‬', 'rus': 'Нун софи́т'}, {'letter': 'ס‬', 'heb': 'סָמֶך‬ְ', 'rus': 'Са́мех'},
+        {'letter': 'ע‬', 'heb': 'עַיִן‬', 'rus': 'А́ин'}, {'letter': 'פּ‬', 'heb': 'פֵּא', 'rus': 'Пей'},
+        {'letter': 'פ‬', 'heb': 'פֵא', 'rus': 'Фей'}, {'letter': 'ף‬', 'heb': 'פֵא סוֹפִית', 'rus': 'Фей софи́т'},
+        {'letter': 'צ‬', 'heb': 'צַדִי', 'rus': 'Ца́ди'}, {'letter': 'ץ‬', 'heb': 'צַדִי סוֹפִית', 'rus': 'Ца́ди софи́т'},
+        {'letter': 'ק‬', 'heb': 'קוֹף', 'rus': 'Куф'}, {'letter': 'ר‬', 'heb': 'רֵישׁ‬', 'rus': 'Рейш'},
+        {'letter': 'שׁ‬', 'heb': 'שִׁין‬', 'rus': 'Шин'}, {'letter': 'שׂ‬‬', 'heb': 'שִׂין‬', 'rus': 'Син'},
+        {'letter': 'ת‬', 'heb': 'תּו‬', 'rus': 'Тав'}
     ];
 
-    var lastLetter = $('#hebrew-letter').text();
-    var rand = getRandInt(0, letter.length);
-    var hebLetter = letter[rand]['he'];
+    let letterArea = '#hebrew-letter';
+    let lastLetter = $(letterArea).text();
+    let randN = getRandInt(0, letters.length);
+    let hebLetter = letters[randN]['letter'];
 
     while (lastLetter === hebLetter) {
-        rand = getRandInt(0, letter.length);
-        hebLetter = letter[rand]['he'];
+        randN = getRandInt(0, letters.length);
+        hebLetter = letters[randN]['letter'];
     }
-    $('#hebrew-letter').html(hebLetter);
-    $('#answ-letter-rus').html(letter[rand]['ru']);
-    /* show button title */
-    $('#answer-btn').val(btnTitleTranslate);
-    /* disable green color of the button if there is */
-    if ($('#answer-btn').hasClass('btn-success')) $('#answer-btn').removeClass('btn-success');
-    /* hide answer */
-    $('#letter-answ').hide();
+
+    let taskStr = hebLetter;
+    let translit = letters[randN]['rus'];
+    let hebrew = letters[randN]['heb'];
+
+    outputTask(taskStr);
+    setBtnTitle(translateBtn, btnTitleTranslate);
+    removeBtnColor(translateBtn, greenColor);  // disable green color of the button if it set before
+    outputTranslation(translit, hebrew);
+    hideTranslation();
 }
 
-function letterAnswerClick() {
-    if ($('#letter-answ').css('display') == 'none') {
-        $('#letter-answ').fadeIn(250);
-        $('#answer-btn').val(btnTitleMore);
-        $('#answer-btn').addClass('btn-success');
-    }
-    else {
-        letterOutput();
-    }
-}
+/** Events **/
+/* Page load */
+page.ready(function() {
+    outputContent();
+});
 
-/* answer button click */
-$('#answer-btn').on('click', function() { letterAnswerClick() });
+/* Click on Translate button */
+translateBtn.on('click', function() { showTranslationOrNewTask(); });
